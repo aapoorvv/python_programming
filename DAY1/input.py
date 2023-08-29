d50dff4c668d0fd19eb88db3a0003abf56a94380
Apoorv Gupta <apoorvg26@gmail.com>
2023-08-29 21:39:53 +0530

    DAY 1

---
+++ DAY1/input.py
@@ -0,0 +1,2 @@
+# print("Hello "+input("What is your name? "))
+print(len(input("What is your name? ")))
---
+++ DAY1/variable.py
@@ -0,0 +1,10 @@
+from multiprocessing.context import SpawnProcess
+
+
+name = input("What is your name? ")
+print(name+name)
+# naming rules:
+#     no spaces 
+#     can use _ and . 
+#     no keywords
+    
