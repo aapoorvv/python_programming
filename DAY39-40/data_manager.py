fce3e2d80402b31e4ddbf47e031dc18a1fd12540
Apoorv Gupta <apoorvg26@gmail.com>
2024-02-03 17:49:29 +0530

    flight deal finder

---
+++ DAY39-40/data_manager.py
@@ -0,0 +1,29 @@
+from pprint import pprint
+import requests
+
+SHEETY_PRICES_ENDPOINT =""
+SHEETY_HEADERS = {"Authorization":""}
+class DataManager:
+
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT,headers=SHEETY_HEADERS)
+        data = response.json()
+        self.destination_data = data["prices"]
+        return self.destination_data
+
+    def update_destination_codes(self):
+        for city in self.destination_data:
+            new_data = {
+                "price": {
+                    "iataCode": city["iataCode"]
+                }
+            }
+            response = requests.put(
+                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
+                json=new_data,
+                headers=SHEETY_HEADERS
+            )
+            print(response.text)
